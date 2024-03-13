8827ac3fca3aef5294d9906e29e1f304dc470501
KirrBrest <lukashchuk.kiryl@yandex.by>
2024-03-13 15:50:23 +0300

    feat: nazvanie_vashego_commita (Wed, Mar 13, 2024 03:50:23 PM)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -287,8 +287,11 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  const data = JSON.parse(json);
+  const result = Object.setPrototypeOf(data, proto);
+  return result;
 }
 
 /**
